2cd100d4f8b77df94db85974e9025c4e55a386c5
Michael Brady <mbrady@royal-22.cs.wisc.edu>
2024-04-14 21:18:01 -0500

    idk

--- p6YEAH/starter_code/ring_buffer.c
+++ p6YEAH/starter_code/ring_buffer.c
@@ -9,31 +9,13 @@
 
 // Mutex for thread safety
 pthread_mutex_t ring_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t full = PTHREAD_COND_INITIALIZER;
 
 int init_ring(struct ring *r)
 {
-    // Yeehaw! Let's get this rodeo started by settin' up the producer head and tail
-    r->p_head = 0;
-    r->p_tail = 0;
 
-    // Can't forget about the consumer head and tail, let's get those in line too
-    r->c_head = 0;
-    r->c_tail = 0;
-
-    // Now we're onto the buffer array, like settin' up targets at the shootin' range
-    for (int i = 0; i < RING_SIZE; i++)
-    {
-        // Start by settin' the request type to somethin' that ain't valid, like a coyote at a cat show
-        r->buffer[i].req_type = -1;
-
-        // Now we initialize the key and value, like loadin' a six-shooter
-        r->buffer[i].k = 0;
-        r->buffer[i].v = 0;
-
-        // Finally, we set up the result offset and ready flag, like plantin' our boots firmly in the stirrups
-        r->buffer[i].res_off = 0;
-        r->buffer[i].ready = 0;
-    }
+    return 0;
 }
 
 void ring_submit(struct ring *r, struct buffer_descriptor *bd)
@@ -42,12 +24,8 @@ void ring_submit(struct ring *r, struct buffer_descriptor *bd)
     pthread_mutex_lock(&ring_mutex);
 
     // Let's take a gander and see if the buffer's chock-full
-    if ((r->p_head + 1) % RING_SIZE == r->p_tail)
-    {
-        // Well, shucks. Buffer's as full as a tick on a hound dog. Better unlock that mutex and skedaddle
-        pthread_mutex_unlock(&ring_mutex);
-        return;
-    }
+    if (r->p_head >= r->p_tail + RING_SIZE)
+        pthread_cond_wait(&full, &ring_mutex);
 
     // Rootin' tootin'! We got some space. Time to add our request to the buffer
     r->buffer[r->p_head] = *bd;
@@ -56,7 +34,7 @@ void ring_submit(struct ring *r, struct buffer_descriptor *bd)
     r->p_head = (r->p_head + 1) % RING_SIZE;
 
     // All done! Time to unlock the mutex and let the next cowboy have a turn
-    pthread_mutex_unlock(&ring_mutex);
+    pthread_cond_broadcast(&empty);
 }
 
 void ring_get(struct ring *r, struct buffer_descriptor *bd)
@@ -65,12 +43,8 @@ void ring_get(struct ring *r, struct buffer_descriptor *bd)
     pthread_mutex_lock(&ring_mutex);
 
     // Let's take a gander and see if the buffer's all empty-like
-    if (r->c_head == r->p_tail)
-    {
-        // Well, shucks. Buffer's as empty as a ghost town. Best unlock the mutex and mosey on out
-        pthread_mutex_unlock(&ring_mutex);
-        return;
-    }
+    if (r->c_head >= r->c_tail)
+        pthread_cond_wait(&empty, &ring_mutex);
 
     // Time to rustle up the request from the buffer
     *bd = r->buffer[r->c_head];
@@ -79,5 +53,5 @@ void ring_get(struct ring *r, struct buffer_descriptor *bd)
     r->c_head = (r->c_head + 1) % RING_SIZE;
 
     // Done did our business with the ring buffer, best unlock the mutex now
-    pthread_mutex_unlock(&ring_mutex);
+    pthread_cond_broadcast(&full);
 }
